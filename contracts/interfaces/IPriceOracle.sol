d862ed0eb08697094c2dfea04369b6ff2623fa40
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-07-05 14:24:46 +0900

    adds note about price oracle decimal

--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 
 // pulled from https://polygonscan.com/address/0xd647a6fc9bc6402301583c91decc5989d8bc382d#readContract
 interface IPriceOracle {
-	function latestAnswer() external view returns (int256);
+	function latestAnswer() external view returns (int256); // returns 8 decimal places
 }
